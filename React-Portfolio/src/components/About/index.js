49e220124005419bc6faad5688cbf7d3392a001e
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-19 01:16:16 -0700

    added about section

--- React-Portfolio/src/components/About/index.js
+++ React-Portfolio/src/components/About/index.js
@@ -8,7 +8,7 @@ const About = () => {
           <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
           <div className="my-2">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+              Hey There! Welcome to my portfolio! My name is Jazmyne Bradley. I graduated from the University Of Arizona with a B.S in Information Science and Technology and a minor in Systems Engineering as well as Africana Studies. When I am get distracted with work on the weekdays, I like to go hiking, going to the gym and trying new foods either at restaurants or daring myself to recreate a dish. You know when people go take a walk to clear their mind or wind down, programming is my "winding down". I used to love solving logic puzzles in elementary school and still to this day. I know it's kind of odd seeing a kid solve logic puzzles rather word searches. However, coding and programming are my logic puzzles. I love the thrill of challenges and figuring out a problem to not only solve, but to develop something! Thanks for visiting my portfolio. I hope you enjoy! 
           </p>
           </div>
         </section>
