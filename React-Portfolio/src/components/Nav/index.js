34870efe6872e52a4312f74e230b7934c2e3c47f
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-19 02:39:03 -0700

    Styling nav

--- React-Portfolio/src/components/Nav/index.js
+++ React-Portfolio/src/components/Nav/index.js
@@ -4,6 +4,31 @@ function Nav() {
 
   return (
     <header>
+  <nav>
+    <a href="/" className="dev">
+      Jazmyne Bradley
+    </a>
+    <a href="#about" className="mx-1 sect">About Me</a>
+    <a href="#portfolio" className="mx-2 sect">Portfolio</a>
+    <a href="#resume" className="mx-2 sect">Resume</a>
+    <a href="#contact" className="mx-2 sect">Contact Me</a>
+    {/* <ul className="flex-row">
+      <li className="mx-2">
+        <a href="#about">
+          About me
+        </a>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+      <li className="mx-2">
+        <span>Portfolio</span>
+      </li>
+      <li className="mx-2">
+        <span>Resume</span>
+      </li>
+    </ul> */}
+  </nav>
     </header>
   );
 }
