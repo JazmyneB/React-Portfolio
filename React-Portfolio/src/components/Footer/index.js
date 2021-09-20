ba2c7bc94ba85f483ece433c17f5491585c4b1da
JazmyneB <bradleyj1@email.arizona.edu>
2021-09-20 00:44:01 -0700

    touch ups

--- React-Portfolio/src/components/Footer/index.js
+++ React-Portfolio/src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {MarkGithubIcon, InfoIcon, MailIcon} from '@primer/octicons-react';
 
 
 const Footer = () => {
     return (
         <footer>
-            <MarkGithubIcon size={16} />
+            <a className='mx-2' href='https://github.com/JazmyneB'><MarkGithubIcon size={55} /></a>
+            <a className='mx-2' href='https://www.linkedin.com/in/jazmynebradley/'><InfoIcon size={55} /></a>
+            <a className='mx-2' href='mailto:bradleyj1@email.arizona.edu'><MailIcon size={55} /></a>
         </footer>
 
     )
